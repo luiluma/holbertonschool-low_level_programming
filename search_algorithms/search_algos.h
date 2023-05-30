670b9ce1647491edc1b7729e517111ebbd7bb0e2
luiluma <luma24lopez@gmail.com>
2023-05-30 13:11:34 -0500

    file library general the prototypes

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,21 +1,13 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+/* LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
-#include "search_algos.h"
+#include <math.h>
 
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+/* PROTOTYPE */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
-}
+#endif
